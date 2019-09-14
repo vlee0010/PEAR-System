381996c331238e7cefffbf2579b325da68ae4756
vlee0010 <vlee0010@student.monash.edu>
2019-09-14 18:22:46 +1000

    add dummy data

--- PEAR/config/schema/dbb.sql
+++ PEAR/config/schema/dbb.sql
@@ -1,12 +1,13 @@
-create table Units
+create table units
 (
-    id int auto_increment,
-    title VARCHAR(1000) not null,
-    semester VARCHAR(1) not null,
-    year DATE not null,
-    constraint Units_pk
-        primary key (id)
+    id int auto_increment
+        primary key,
+    title varchar(1000) not null,
+    code varchar(10) not null,
+    semester varchar(1) not null,
+    year year not null
 );
+
 
 create table units_users
 (
@@ -97,6 +98,56 @@ create table responses
     constraint responses_users_id_fk
         foreign key (user_id) references users (id)
 );
+
+-- Insert some dummy data to database for testing
+INSERT INTO units(id, title, code, semester, year) VALUES
+(1, 'Industry Experience','FIT3047','1',2019),
+(2, 'Industry Experience','FIT3047','2', 2019),
+(3, 'Ios application development', 'FIT3178','A',2019),
+(4, 'Ios application development','FIT3178','A',2020);
+
+INSERT INTO teams(id_, name) VALUES
+(1,'Team123'), (2,'Team122');
+
+-- Jianheng Huang, team 123, number of mem 2
+INSERT INTO teams_users(user_id, team_id, number) VALUES
+(6,1,2),(7,1,2);
+
+-- Jianheng Huang enrols in FIT3047 sem1 2019, FIT3178 semA 3178
+INSERT INTO units_users(unit_id, user_id) VALUES
+(1,6),(3,6),
+(1,7),(3,7);
+
+-- FIT3047 sem1 2019 has 3 peer reviews listed below
+INSERT INTO peer_reviews(id, date_start, date_end, date_reminder, title, unit_id) VALUES
+(1,'2019-07-08 02:30:14', '2019-07-14 02:30:14','2018-07-12 02:30:14','Industry Experience Iteration 1 Peer Review',1),
+(2,'2019-09-08 02:30:14', '2019-09-14 02:30:14','2018-09-12 02:30:14','Industry Experience Iteration 2 Peer Review',1),
+(3,'2019-10-08 02:30:14', '2019-10-14 02:30:14','2018-10-12 02:30:14','Industry Experience Iteration 3 Peer Review',1);
+
+-- 3 questions for FIT3047 sem1 2019 iteration1 peer reviews
+INSERT INTO questions(id, question, peer_review_id) VALUES
+(1,'Contribute to the teamwork',1),
+(2,'Keeping the team on track',1),
+(3,'Interactive with the team',1);
+
+-- Answer score 1-5
+INSERT INTO answers(id, answer) VALUES
+(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5');
+
+-- Jianheng Huang answer FIT3047 sem1 2019 iteration1 q1:5, q2:4, q3:1
+INSERT INTO responses(id, date_response, user_id, question_id, answer_id) VALUES
+(1,'2018-07-12 02:30:14',6,1,5),
+(2,'2018-07-12 02:30:14',6,2,4),
+(3,'2018-07-12 02:30:14',6,3,1);
+
+-- FIT3047 sem1 2019 iteration1 peer reviews assigns to team123, team122
+INSERT INTO peer_reviews_teams(peer_review_id, team_id) VALUES
+(1,1),(1,2);
+
+
+
+
+
 
 
 
