5088baf21086af0bd8423f7659e136693d642e64
vlee0010 <vlee0010@student.monash.edu>
2019-12-11 23:17:34 +1100

    add email template page

---
+++ PEAR/src/Model/Table/EmailTable.php
@@ -0,0 +1,100 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Email Model
+ *
+ * @property \App\Model\Table\UnitsTable&\Cake\ORM\Association\BelongsTo $Units
+ *
+ * @method \App\Model\Entity\Email get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Email newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Email[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Email|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Email saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Email patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Email[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Email findOrCreate($search, callable $callback = null, $options = [])
+ */
+class EmailTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('email');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Units', [
+            'foreignKey' => 'unit_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('sender')
+            ->maxLength('sender', 100)
+            ->requirePresence('sender', 'create')
+            ->notEmptyString('sender');
+
+        $validator
+            ->scalar('header')
+            ->maxLength('header', 100)
+            ->requirePresence('header', 'create')
+            ->notEmptyString('header');
+
+        $validator
+            ->scalar('emailSubject')
+            ->maxLength('emailSubject', 100)
+            ->allowEmptyString('emailSubject');
+
+        $validator
+            ->scalar('message')
+            ->maxLength('message', 1000)
+            ->allowEmptyString('message');
+
+        $validator
+            ->scalar('fromSender')
+            ->maxLength('fromSender', 100)
+            ->requirePresence('fromSender', 'create')
+            ->notEmptyString('fromSender');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['unit_id'], 'Units'));
+
+        return $rules;
+    }
+}
