964b6b7e7d46140a0cad413046c99537674e1f91
vlee0010 <vlee0010@student.monash.edu>
2019-12-17 15:24:05 +1100

    change link for security testing

--- PEAR/src/Controller/UsersController.php
+++ PEAR/src/Controller/UsersController.php
@@ -384,7 +384,7 @@ class UsersController extends AppController
                 $body = 'Hi, ' . $myFirstName . ' ' . $myLastName;
                 $body .= "<br><br>Please Click the link below to verify your registration.";
 //                $body .= "<br><br><a href=http://localhost:8888/PEAR/PEAR/users/verification/".$myToken.">Verification Link</a>" ;
-                $body .= "<br><br><a href=http://ie.infotech.monash.edu/team123/iteration3/team123-app/pear/team123-app/PEAR/users/verification/" . $myToken . ">Verification Link</a>";
+                $body .= "<br><br><a href=http://ie.infotech.monash.edu/team123/NandS/team123-app/PEAR/users/verification/" . $myToken . ">Verification Link</a>";
 
 
                 $email = new Email('default');
